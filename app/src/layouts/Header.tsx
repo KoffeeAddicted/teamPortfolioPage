c989ea5f9689c8365c7e8b15ed941a0723541494
Hao Dang <ericdang2709@gmail.com>
2023-04-03 22:33:56 +0700

    handle add logo and style for header

--- app/src/layouts/Header.tsx
+++ app/src/layouts/Header.tsx
@@ -1,8 +1,28 @@
+import coffee_outline from '../assets/images/logos/coffee_outline.png';
+import coffee_bold from '../assets/images/logos/coffee_bold.png';
+import { BsTelephone } from 'react-icons/bs';
+import { AiOutlineMail } from 'react-icons/ai';
 const Header = () => {
-    return (
-        <div>Header</div>
-    )
-}
-
+  return (
+    <div className='w-full h-[20vh] bg-blue-500 flex items-center items-between'>
+      <div className='w-full h-fit flex items-center justify-between'>
+        <div id='cf' className='ml-10'>
+          <img className='top' src={coffee_outline} alt='logo' />
+          <img className='bottom' src={coffee_bold} alt='logo_hover' />
+        </div>
+        <div className='ml-[100px] flex gap-[100px]'>
+          <div>Home</div>
+          <div>Contact</div>
+          <div>About</div>
+        </div>
+        <div className='flex gap-10 mr-10 items-center'>
+          <BsTelephone />
+          <AiOutlineMail />
+          <div>Button Slider</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
