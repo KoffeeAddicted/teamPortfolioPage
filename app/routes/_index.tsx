1a8cd48cba371f165522bc622860d3dbd39ee858
Hao Dang <ericdang2709@gmail.com>
2023-04-12 22:23:03 +0700

    handle fix index routes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,6 +4,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <ContainerIntro />
+      <div className='w-full h-[200vh]'>Container</div>
     </div>
   );
 }
