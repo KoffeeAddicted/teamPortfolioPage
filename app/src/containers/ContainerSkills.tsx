963c057376e8067d8d1b86f506dfd9e2d458e47e
Hao Dang <ericdang2709@gmail.com>
2023-04-12 23:14:59 +0700

    change without i18next

--- app/src/containers/ContainerSkills.tsx
+++ app/src/containers/ContainerSkills.tsx
@@ -1,37 +1,32 @@
-import { useTranslation } from 'react-i18next';
 import Marquee from 'react-fast-marquee';
 
 import { TypographySkill } from '../components';
 import { skillIcons } from '../data/skill';
 
-export let handle = { i18n: 'skills' };
-
 const ContainerSkills = () => {
-  let { t } = useTranslation('skills');
-
   let skillKeywords = [
     {
       style: 'text-tortilla',
-      content: t('ul-item1'),
+      content: 'generating',
     },
     {
       style: 'text-tawny',
-      content: t('ul-item2'),
+      content: 'maintaining',
     },
     {
       style: 'text-goldenrod',
-      content: t('ul-item3'),
+      content: 'optimizing',
     },
   ];
   return (
     <div className='w-full h-[120vh]'>
       <div className='w-full flex h-[50%] items-center justify-center'>
         <div className="font-['MuseoModernoThicc'] py-5 text-center text-[30px] sm:text-[40px] lg:text-[55px] w-[80%] lg:w-[60%]">
-          {t('introduction')}
+          Our team is an expertise at
           <br />
-          <TypographySkill ulItems={skillKeywords} linkingText={t('project')} />
+          <TypographySkill ulItems={skillKeywords} linkingText='project' />
           <span className='font-["MuseoModernoSemiBold"] text-[12px] sm:text-[20px] lg:text-[30px]'>
-            {t('subtitle')}
+            with modern frameworks and languages
           </span>
         </div>
       </div>
