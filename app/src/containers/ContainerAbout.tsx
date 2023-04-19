48683fb9a81f2a376f7a1e6de299d2d952f22af8
Hao Dang <ericdang2709@gmail.com>
2023-04-19 18:57:54 +0700

    handle set multi language text

--- app/src/containers/ContainerAbout.tsx
+++ app/src/containers/ContainerAbout.tsx
@@ -1,10 +1,12 @@
 import { CardProfile } from '../components';
+import { useT } from '../hooks/usei18n';
 
 const ContainerAbout = () => {
+  let t = useT('translation');
   return (
     <div className='w-full h-fit text-[30px] sm:text-[40px] lg:text-[55px] font-["MuseoModernoThicc"]'>
       <div className='h-fit w-full flex justify-center pb-[50px]'>
-        <div>About Us</div>
+        <div>{t('about.title')}</div>
       </div>
       <div className='w-full pb-[200px] flex gap-[20px] justify-center flex-wrap'>
         <CardProfile />
--- public/locales/en.ts
+++ public/locales/en.ts
@@ -1,3 +1,24 @@
 export default {
-  header: { home: 'Home' },
+  header: {
+    home: 'Home',
+    contact: 'Contact',
+    about: 'About',
+  },
+  skills: {
+    intro: 'Our team is an expertise at',
+    advantage1: 'generating',
+    advantage2: 'maintaining',
+    advantage3: 'optimizing',
+    projects: 'projects',
+    frameworks: 'with modern frameworks and languages',
+  },
+  about: {
+    title: 'About Us',
+  },
+  contact: {
+    title: 'Contact',
+    email: '',
+    phone: '',
+    address: '115/60 Tran Dinh Xu, District 1, Ho Chi Minh city',
+  },
 };
