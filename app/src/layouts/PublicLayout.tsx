89cc9ad348c9808ed9712c34177628cf30d97732
Chi hao <ericdang@gmail.com>
2023-04-22 00:14:49 +0700

    handle init loading page

--- app/src/layouts/PublicLayout.tsx
+++ app/src/layouts/PublicLayout.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from 'react';
 import Header from './Header';
 
 const PublicLayout = (props: any) => {
   const { children } = props;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div className='relative'>
-      <Header />
-      <div>{children}</div>
+      <div
+        className={`w-[100vw] h-[100vh] ${
+          loading === false && 'hidden'
+        } overflow-hidden z-[9999] absolute bg-white`}
+      >
+        Loadingggg....
+      </div>
+
+      <>
+        <Header />
+        <div>{children}</div>
+      </>
     </div>
   );
 };
