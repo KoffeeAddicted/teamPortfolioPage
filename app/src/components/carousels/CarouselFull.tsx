a5834367e1591aae335cbfe9e7c0450de66fcd8d
vanthinh <thinh.tran@rozitek.com>
2023-04-23 11:25:34 +0700

    Update img position in carousel

--- app/src/components/carousels/CarouselFull.tsx
+++ app/src/components/carousels/CarouselFull.tsx
@@ -5,6 +5,11 @@ import test from '../../assets/images/backgrounds/test.png';
 import carsouselPic2 from '../../assets/images/backgrounds/carousel-pic2.png';
 import carsouselPic3 from '../../assets/images/backgrounds/carousel-pic3.png';
 import carsouselPic4 from '../../assets/images/backgrounds/carousel-pic4.png';
+import carsouselPic5 from '../../assets/images/backgrounds/carousel-pic5.png';
+import carsouselPic6 from '../../assets/images/backgrounds/carousel-pic6.png';
+import carsouselPic7 from '../../assets/images/backgrounds/carousel-pic7.png';
+import carsouselPic8 from '../../assets/images/backgrounds/carousel-pic8.png';
+
 import FrameCarousel from '../farmes/FrameCarousel';
 
 const CarouselFull = () => {
@@ -12,7 +17,7 @@ const CarouselFull = () => {
     <div className='w-full h-[100vh] flex justify-center items-center bg-slate-200'>
       <Carousel
         //style={{ zIndex: '0' }}
-        auto={true}
+        auto={false}
         loop
         frames={[
           <FrameCarousel
@@ -21,8 +26,9 @@ const CarouselFull = () => {
             nam ipsa dolores pariatur quisquam quis error quibusdam dicta
             mollitia incidunt cum aliquam totam omnis exercitationem ad et,
             eum, in consequuntur?'
-            image={test}
+            image={carsouselPic5}
             background='bg-[Tomatoe]'
+            image2={carsouselPic8}
           />,
           <FrameCarousel
             title='Subtitle 1'
--- app/src/components/farmes/FrameCarousel.tsx
+++ app/src/components/farmes/FrameCarousel.tsx
@@ -3,24 +3,35 @@ interface IFrameCarouselProps {
   description: string;
   image: string;
   background: string;
+  image2?: string;
 }
 const FrameCarousel = ({
   title,
   description,
   image,
   background,
+  image2,
 }: IFrameCarouselProps) => {
   return (
     <div
-      className={`h-full w-full flex align-center ${background}`}
+      className={`h-full w-full flex flex-col justify-center sm:justify-normal sm:flex-row align-center ${background}`}
       // style={{
       //   backgroundColor: 'Tomatoe',
       // }}
     >
-      <div className='w-1/2 h-full flex justify-center items-center'>
-        <img src={image} alt='Image 1' className='w-[600px] object-cover' />
+      <div className='w-full sm:w-1/2 h-full flex justify-center items-center'>
+        <div className='w-fit relative'>
+          <img src={image} alt='Image 1' className='w-[600px] object-cover' />
+          {image2 && (
+            <img
+              src={image2}
+              alt='Image 2'
+              className='object-cover absolute top-[10%] right-0 left-[50%] w-[200px]'
+            />
+          )}
+        </div>
       </div>
-      <div className='w-1/2 flex flex-col justify-center h-full'>
+      <div className='w-full sm:w-1/2 flex flex-col justify-center h-full'>
         <div>
           <div className='font-["MuseoModernoSemiBold"] text-[12px] sm:text-[20px] lg:text-[30px]'>
             {title}
